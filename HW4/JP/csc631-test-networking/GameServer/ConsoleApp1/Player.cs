28b1978cf58fa009c3c412201cb1ff0560498ec7
John Paul Apolinar <john.apolinar@gmail.com>
2021-03-23 22:56:55 -0700

    Player movement completely synched!
    - The playermodels are through the floor though. Whoops.

--- HW4/JP/csc631-test-networking/GameServer/ConsoleApp1/Player.cs
+++ HW4/JP/csc631-test-networking/GameServer/ConsoleApp1/Player.cs
@@ -27,27 +27,27 @@ namespace GameServer
             inputs = new bool[4]; 
         }
 
-        public void Update() 
+        public void Update()
         {
             Vector2 _inputDirection = Vector2.Zero;
-            if (inputs[0]) 
+            if (inputs[0])
             {
-                _inputDirection.Y += 1; 
-            }            
-            if (inputs[1]) 
+                _inputDirection.Y += 1;
+            }
+            if (inputs[1])
             {
-                _inputDirection.Y -= 1; 
-            }           
-            if (inputs[2]) 
+                _inputDirection.Y -= 1;
+            }
+            if (inputs[2])
             {
-                _inputDirection.X += 1; 
-            }            
-            if (inputs[3]) 
+                _inputDirection.X += 1;
+            }
+            if (inputs[3])
             {
-                _inputDirection.X -= 1; 
+                _inputDirection.X -= 1;
             }
 
-            Move(_inputDirection); 
+            Move(_inputDirection);
         }
 
         private void Move(Vector2 _inputDirection)
@@ -55,7 +55,7 @@ namespace GameServer
             Vector3 _forward = Vector3.Transform(new Vector3(0, 0, 1), rotation);
             Vector3 _right = Vector3.Normalize(Vector3.Cross(_forward, new Vector3(0, 1, 0)));
 
-            Vector3 _moveDirection = _right * _inputDirection.X * _forward * _inputDirection.Y;
+            Vector3 _moveDirection = _right * _inputDirection.X + _forward * _inputDirection.Y;
             position += _moveDirection * moveSpeed;
 
             ServerSend.PlayerPosition(this);
