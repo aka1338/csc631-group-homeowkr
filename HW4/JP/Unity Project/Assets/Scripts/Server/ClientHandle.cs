800f4b32873b099f480febb22361874d4b6047e9
John Paul Apolinar <john.apolinar@gmail.com>
2021-03-23 21:15:49 -0700

    Got UDP to work!

--- HW4/JP/Unity Project/Assets/Scripts/Server/ClientHandle.cs
+++ HW4/JP/Unity Project/Assets/Scripts/Server/ClientHandle.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using UnityEngine;
 
 public class ClientHandle : MonoBehaviour
@@ -13,8 +14,10 @@ public class ClientHandle : MonoBehaviour
         Client.instance.myId = _myId;
 
         ClientSend.WelcomeReceived();
+
+        Client.instance.udp.Connect(((IPEndPoint)Client.instance.tcp.socket.Client.LocalEndPoint).Port);
     }
-    
+
     public static void UDPTest(Packet _packet)
     {
         string _msg = _packet.ReadString();
