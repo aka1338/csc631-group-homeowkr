d5cf9effb6427a56163cea6c2770251ebd2261c6
John Paul Apolinar <john.apolinar@gmail.com>
2021-03-24 21:10:19 -0700

    Added Disconnect functionality to Main scene

--- HW4/JP/Unity Project/Assets/Scripts/Server/Client.cs
+++ HW4/JP/Unity Project/Assets/Scripts/Server/Client.cs
@@ -14,8 +14,9 @@ public class Client : MonoBehaviour
     public int port = 26950;
     public int myId = 0;
     public TCP tcp;
-    public UDP udp; 
+    public UDP udp;
 
+    private bool isConnected = false; 
     private delegate void PacketHandler(Packet _packet);
     private static Dictionary<int, PacketHandler> packetHandlers;
 
@@ -38,10 +39,16 @@ public class Client : MonoBehaviour
         udp = new UDP();
     }
 
+    private void OnApplicationQuit()
+    {
+        Disconnect(); 
+    }
+
     public void ConnectToServer()
     {
         InitializeClientData();
 
+        isConnected = true; 
         tcp.Connect();
     }
 
@@ -103,7 +110,7 @@ public class Client : MonoBehaviour
                 int _byteLength = stream.EndRead(_result);
                 if (_byteLength <= 0)
                 {
-                    // TODO: disconnect
+                    instance.Disconnect(); 
                     return;
                 }
 
@@ -115,8 +122,18 @@ public class Client : MonoBehaviour
             }
             catch
             {
-                // TODO: disconnect
+                Disconnect(); 
             }
+        }
+
+        private void Disconnect()
+        {
+            instance.Disconnect();
+
+            stream = null;
+            receivedData = null;
+            receiveBuffer = null;
+            socket = null; 
         }
 
         private bool HandleData(byte[] _data)
@@ -214,7 +231,7 @@ public class Client : MonoBehaviour
 
                 if (_data.Length < 4)
                 {
-                    // TODO: disconnect
+                    instance.Disconnect(); 
                     return;
                 }
 
@@ -222,7 +239,7 @@ public class Client : MonoBehaviour
             }
             catch
             {
-                // TODO: disconnect
+                Disconnect(); 
             }
         }
 
@@ -243,6 +260,14 @@ public class Client : MonoBehaviour
                 }
             });
         }
+
+        private void Disconnect() 
+        {
+            instance.Disconnect();
+
+            endPoint = null;
+            socket = null; 
+        }
     }
 
     private void InitializeClientData()
@@ -257,5 +282,17 @@ public class Client : MonoBehaviour
         };
 
         Debug.Log("Initialized packets.");
+    }
+
+    private void Disconnect()
+    {
+        if (isConnected)
+        {
+            isConnected = false;
+            tcp.socket.Close();
+            udp.socket.Close();
+
+            Debug.Log("Disconnected from server."); 
+        }
     }
 }
