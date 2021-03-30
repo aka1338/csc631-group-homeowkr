a13ecd06d0391a5474a2c37ee86d6bf237ad62d7
John Paul Apolinar <john.apolinar@gmail.com>
2021-03-30 13:28:00 -0700

    Fixed FINAL folder

---
+++ HW4/Individual Branches/MZ/csc631-test-networking/Unity Project/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public Transform player;
+    public Vector3 offset;
+    public Vector3 topDown;
+    public float y;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + offset;
+        if (Input.GetKey("z"))
+        {
+            transform.position = player.position + topDown;
+            transform.rotation = Quaternion.Euler(y,0,0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+}
---
+++ HW4/Individual Branches/MZ/csc631-test-networking/Unity Project/Assets/Scripts/SunLight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunLight : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey("q"))
+        {
+            transform.rotation = Quaternion.Euler(transform.eulerAngles.x + 1.0f, transform.eulerAngles.y, transform.eulerAngles.z);
+        }
+
+        if (Input.GetKey("e"))
+        {
+            transform.rotation = Quaternion.Euler(transform.eulerAngles.x - 1.0f, transform.eulerAngles.y, transform.eulerAngles.z);
+        }
+    }
+}
